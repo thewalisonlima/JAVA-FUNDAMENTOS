6ad464e76edacaae1d5b50d86383c0eef7dcf7e8
npcwalison <gwalisonls@gmail.com>
2025-11-15 11:00:26 -0300

    Condicionais

--- src/basico/SintaxeBasica.java
+++ src/basico/SintaxeBasica.java
@@ -2,33 +2,39 @@ package basico;
 
 public class SintaxeBasica {
     public SintaxeBasica() {
-        // Variaveis & Tipos Primitivos
+        //============================================
+        // VARIAVEIS & TIPOS PRIMITIVOS
+        //============================================
 
         // NÚMEROS INTEIROS
-        byte bb = 2; // Ocupa 8 bits => -128 a 127
-        short ss = 3; // Ocupa 16 bits => -32.768 a 32.767
-        int ii = 5; // Ocupa 32 bits => -2.147.483.648 a 2.147.483.647
-        long ll = 10; // Ocupa 64 bits => -9.223.372.036.854.775.808 a 9.223.372.036.854.775.807
+        byte bb = 100; // Ocupa 8 bits => -128 a 127
+        short ss = 10600; // Ocupa 16 bits => -32.768 a 32.767
+        int ii = 102000; // Ocupa 32 bits => -2.147.483.648 a 2.147.483.647
+        long ll = 103400L; // Ocupa 64 bits => -9.223.372.036.854.775.808 a 9.223.372.036.854.775.807
 
         // NÚMEROS DECIMAIS
-        float rr = 4.2f; // 32 bits => precisão simples
-        double dd = 3.5; // 64 bits => precisão dupla
+        float rr = 40.2f; // 32 bits => precisão simples
+        double dd = 33.5; // 64 bits => precisão dupla
 
         // OUTROS...
         String st = "George"; // Palavras ou Frases
-        char cc = 'a'; // Representa um único caractere
-        boolean teste = true; // true(verdadeiro) ou false(falso)
+        char cc = 'A'; // Representa um único caractere
+        boolean bool = true; // true(verdadeiro) ou false(falso)
 
-        System.out.println(bb);
-        System.out.println(ss);
-        System.out.println(ii);
-        System.out.println(ll);
+        System.out.print("byte:" + bb + "\n" + "short:" + ss + "\n" + "int:" + ii + "\n" + "long:" + ll + "\n" + "float:" + rr + "\n" + "double:" + dd + "\n" + "String:" + st + "\n" + "char:" + cc + "\n" + "boolean:" + bool + "\n");
 
-        System.out.println(rr);
-        System.out.println(dd);
 
-        System.out.println(st);
-        System.out.println(cc);
-        System.out.println(teste);
+        //============================================
+        // CONDICIONAIS
+        //============================================
+
+
+        if ( dd != 0) {
+            System.out.print("Tem valor");
+        } else if (bool) {
+            System.out.print("Verdadeiro");
+        } else {
+            System.out.print("Falso");
+        }
     }
 }
