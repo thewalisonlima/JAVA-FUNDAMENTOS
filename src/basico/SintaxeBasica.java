aa7e030ca15ec2e8617d5205ef2dea5bf54e97b8
npcwalison <gwalisonls@gmail.com>
2025-11-17 15:59:09 -0300

    ArrayLists & Loops de Repetição

--- src/basico/SintaxeBasica.java
+++ src/basico/SintaxeBasica.java
@@ -1,5 +1,7 @@
 package basico;
 
+import java.util.ArrayList;
+
 public class SintaxeBasica {
     public SintaxeBasica() {
         //============================================
@@ -30,11 +32,61 @@ public class SintaxeBasica {
 
 
         if ( dd != 0) {
-            System.out.print("Tem valor");
+            System.out.print("Tem valor \n");
         } else if (bool) {
-            System.out.print("Verdadeiro");
+            System.out.print("Verdadeiro \n");
         } else {
-            System.out.print("Falso");
+            System.out.print("Falso \n");
+        }
+
+        //============================================
+        // VETORES
+        //============================================
+
+
+        int[] colecaoDeInteiros = {1 ,2 ,3 , 4, 5};
+
+        System.out.print(colecaoDeInteiros[1] + "\n");
+        System.out.print(colecaoDeInteiros.length + "\n");
+
+
+        //============================================
+        // ARRAY LIST (é uma classe)
+        //============================================
+
+
+        ArrayList<String> nomes = new ArrayList<>();
+
+        nomes.add("Walison"); // Adicionando valor
+        nomes.add("Lucas");
+        nomes.add("Maria");
+        nomes.add("Lucas");
+
+        System.out.print(nomes.get(0) + "\n"); // Pesquisa item
+        nomes.remove(1); // Remove item
+
+
+        //============================================
+        // LOOPS FOR & WHILE
+        //============================================
+
+        
+        // Repetidor com contador
+        for(int i = 0; i < nomes.size(); i++) {
+            System.out.print(nomes.get(i) + "\n");
+        }
+        System.out.print("\n");
+
+        boolean modelo = true;
+        int counter = 5;
+
+        // Repetidor com condição
+        while (modelo) {
+            counter--;
+            System.out.print(counter);
+            if(counter <= 0) {
+                modelo = false;
+            }
         }
     }
 }
